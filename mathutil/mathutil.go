e8777d3819936d7b426026f62db0afc1a6e9cc05
b1ron <goldingbyron@gmail.com>
2025-03-06 04:07:47 +0100

    fix ProbalisticPrimeTest

--- mathutil/mathutil.go
+++ mathutil/mathutil.go
@@ -26,6 +26,7 @@ func GCD(a, b int) int {
 // https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
 // It runs in polynomial time.
 func ProbalisticPrimeTest(n int) bool {
+	a := 0
 	d := 3
 	s := 0
 	x := 0
@@ -37,13 +38,14 @@ func ProbalisticPrimeTest(n int) bool {
 	}
 
 	// [2, min(n − 2, ⌊2(ln n)^2⌋)]
-	bound := math.Min(float64(n-2), math.Floor(2*math.Pow(math.Log(float64(n)), 2)))
-	for a := range int(bound) {
+	maxBase := math.Min(float64(n-2), math.Floor(2*math.Pow(math.Log(float64(n)), 2)))
+	for range int(maxBase) {
 		// compute a^d mod n
+		a = rand.Intn(int(maxBase)-2) + 2
 		x = Pow(a, d) % n
 		// handle later
 		if x == 1 || x == n-1 {
-			break
+			continue
 		}
 		for range s {
 			y = Pow(x, 2) % n
---
+++ mathutil/mathutil_test.go
@@ -0,0 +1,23 @@
+package mathutil
+
+import "testing"
+
+func TestProbalisticPrimeTest(t *testing.T) {
+	tests := []struct {
+		n    int
+		want bool
+	}{
+		// {1, false},
+		// {2, true},
+		// {3, true},
+		// {4, false},
+		{5, true},
+		{6, false},
+		{997, true},
+	}
+	for _, test := range tests {
+		if got := ProbalisticPrimeTest(test.n); got != test.want {
+			t.Errorf("probalisticPrimeTest(%d) = %t, want %t", test.n, got, test.want)
+		}
+	}
+}
