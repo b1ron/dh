080cfec02b61f5b2d4abf842f823973516bd7b02
b1ron <goldingbyron@gmail.com>
2025-03-06 04:43:26 +0100

    fix ProbalisticPrimeTest

--- mathutil/mathutil.go
+++ mathutil/mathutil.go
@@ -2,15 +2,19 @@
 package mathutil
 
 import (
-	"math"
 	"math/rand"
 )
 
-// helper to avoid annoying float64 conversion
-func Pow(x, y int) int {
+// Pow computes x^y using modular exponentiation.
+func Pow(x, y, mod int) int {
 	res := 1
-	for range y {
-		res *= x
+	x = x % mod
+	for y > 0 {
+		if y%2 == 1 {
+			res = (res * x) % mod
+		}
+		x = (x * x) % mod
+		y /= 2
 	}
 	return res
 }
@@ -26,40 +30,45 @@ func GCD(a, b int) int {
 // https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
 // It runs in polynomial time.
 func ProbalisticPrimeTest(n int) bool {
+	if n < 2 {
+		return false
+	}
+
+	if n == 2 || n == 3 {
+		return true
+	}
+
 	a := 0
-	d := 3
+	d := n - 1
 	s := 0
 	x := 0
-	y := 0
 
 	for d%2 == 0 {
 		d /= 2
 		s++
 	}
 
-	// [2, min(n − 2, ⌊2(ln n)^2⌋)]
-	maxBase := math.Min(float64(n-2), math.Floor(2*math.Pow(math.Log(float64(n)), 2)))
-	for range int(maxBase) {
+	for range 10 {
+		a = rand.Intn(n-3) + 2
 		// compute a^d mod n
-		a = rand.Intn(int(maxBase)-2) + 2
-		x = Pow(a, d) % n
+		x = Pow(a, d, n)
 		// handle later
 		if x == 1 || x == n-1 {
 			continue
 		}
 		for range s {
-			y = Pow(x, 2) % n
+			x = Pow(x, 2, n) % n
+			if x == n-1 {
+				break
+			}
 		}
-		if y == 1 && x != 1 && x != n-1 {
+		if x != 1 && x != n-1 {
 			return false
-		} else {
-			x = y
-			break
 		}
 	}
 
 	// if none of the bases a detect compositeness, we assume n is probably prime.
-	return y == 1
+	return true
 }
 
 func GeneratePrime(bitsize int) int {
--- mathutil/mathutil_test.go
+++ mathutil/mathutil_test.go
@@ -7,13 +7,23 @@ func TestProbalisticPrimeTest(t *testing.T) {
 		n    int
 		want bool
 	}{
-		// {1, false},
-		// {2, true},
-		// {3, true},
-		// {4, false},
+		{1, false},
+		{2, true},
+		{3, true},
+		{4, false},
 		{5, true},
 		{6, false},
+		{7, true},
+		{8, false},
+		{9, false},
+		{10, false},
+		{11, true},
+		{12, false},
+		{13, true},
 		{997, true},
+		{999, false},
+		{1000, false},
+		{1001, false},
 	}
 	for _, test := range tests {
 		if got := ProbalisticPrimeTest(test.n); got != test.want {
